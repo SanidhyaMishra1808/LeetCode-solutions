aab9fb56eb8359e6e48ed8c88f03879eb0a4f83b
Sanidhya Mishra <114796705+SanidhyaMishra1808@users.noreply.github.com>
2024-04-20 21:53:29 +0530

    Time: 3 ms (95.70%) | Memory: 8.5 MB (60.38%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -1,29 +1,45 @@
 class Solution {
-public:
-    bool areAnagram(string &s1,string &s2){
-        int count[256] = {0};
-        for(int i=0; i<s1.size(); i++){
-            count[s1[i]]++;
-            count[s2[i]]--;
+    private:
+    bool checkEqual( int a[26], int b[26]){
+        for(int i=0;i<26;i++){
+             if( a[i]!= b[i])
+        return 0;
         }
-        for(int i=0; i<256; i++){
-            if(count[i]!=0){
-                return false;
-            }
-        }return true;
+        return 1; 
     }
+public:
     bool checkInclusion(string s1, string s2) {
-        int i=0;
-        if(s1.size() > s2.size()){
-            return false;
+        //count of string 1
+        int count1[26]={0};
+        for(int i=0;i<s1.length();i++){
+            int index=s1[i]-'a';
+            count1[index]++;
         }
-        while(i <= s2.size()-s1.size()){
-            string s = s2.substr(i,s1.size());
-            if(areAnagram(s,s1)){
-                return true;
-            }
+        //count of string 2 of window size of string 2
+        int count2[26]={0};
+        int windowsize=s1.length();
+        int i=0;
+        while(i<windowsize && i<s2.length()){
+            int index=s2[i]-'a';
+            count2[index]++;
             i++;
         }
-        return false;
+        if(checkEqual(count1,count2))
+            return 1;
+        
+        //next window process
+        while(i<s2.length())
+        {
+            char newchar=s2[i];
+            int index=newchar -'a';
+            count2[index]++;
+            char oldchar=s2[i-windowsize];
+             index=oldchar -'a';
+            count2[index]--;
+            i++;
+            if(checkEqual(count1,count2))
+            return 1;
+        }
+      return 0;  
     }
 };
