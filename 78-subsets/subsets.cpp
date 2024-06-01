c0014362e4d1c73394bae52eb9bfcf03c4c1268c
Sanidhya Mishra <114796705+SanidhyaMishra1808@users.noreply.github.com>
2024-06-01 12:01:50 +0530

    Time: 2 ms (51.80%) | Memory: 8.5 MB (46.19%) - LeetSync

--- 78-subsets/subsets.cpp
+++ 78-subsets/subsets.cpp
@@ -1,23 +1,17 @@
 class Solution {
 public:
-void solution(vector<int>nums, vector<int>output, int i, vector<vector<int>>&ans){
-    //basee case
-    if(i >= nums.size()){
-        ans.push_back(output);
-        return;
-    }
-    //exclude condition
-    solution(nums,output,i+1,ans);
-    //recursive call (include)
-    int element =  nums[i];
-    output.push_back(element);
-    solution(nums,output,i+1,ans);
-}
     vector<vector<int>> subsets(vector<int>& nums) {
-       vector<vector<int>>ans;
-       vector<int>output;
-       int i = 0;
-       solution(nums,output,i, ans);
+        vector<vector<int>>ans;
+        int n = nums.size();
+        for(int i=0; i<pow(2,n); i++){
+            vector<int>v;
+            for(int j=0; j<n; j++){
+                if((1<<j)&i){        //left shift
+                    v.push_back(nums[j]);
+                }
+            }
+            ans.push_back(v);
+       }
        return ans; 
     }
 };
