d7da1e4a65ed2dfb6e34c3d3bb7d6f3bab978716
Sanidhya Mishra <114796705+SanidhyaMishra1808@users.noreply.github.com>
2024-06-16 23:09:04 +0530

    Time: 171 ms (54.65%) | Memory: 130.6 MB (18.00%) - LeetSync

--- 234-palindrome-linked-list/palindrome-linked-list.cpp
+++ 234-palindrome-linked-list/palindrome-linked-list.cpp
@@ -1,39 +1,61 @@
+#include<vector>
 class Solution {
-public:
-ListNode* reverse(ListNode* head) {
-    if(head == NULL || head -> next == NULL){
-            return head;
+private:
+bool isPalindrome(vector<int>&arr){
+    int s = 0;
+    int n = arr.size();
+    int e = n-1;
+    while(s<e){
+        if(arr[s] != arr[e]){
+            return false;
         }
-        ListNode* curr = head;
-        ListNode* forward = NULL;
-        ListNode* prev = NULL;
-        while(curr != NULL){
-            forward = curr -> next;
-            curr -> next = prev;
-            prev = curr;
-            curr = forward;
-        }
-        return prev;
+        s++;
+        e--;
+    }
+    return true;
 }
+public:
+// ListNode* reverse(ListNode* head) {
+//     if(head == NULL || head -> next == NULL){
+//             return head;
+//         }
+//         ListNode* curr = head;
+//         ListNode* forward = NULL;
+//         ListNode* prev = NULL;
+//         while(curr != NULL){
+//             forward = curr -> next;
+//             curr -> next = prev;
+//             prev = curr;
+//             curr = forward;
+//         }
+//         return prev;
+// }
     bool isPalindrome(ListNode* head) {
-        ListNode* slow = head;
-        ListNode* fast = head;
-        while(fast -> next != NULL && fast -> next -> next != NULL){
-            slow = slow -> next;
-            fast = fast -> next -> next;
+        // ListNode* slow = head;
+        // ListNode* fast = head;
+        // while(fast -> next != NULL && fast -> next -> next != NULL){
+        //     slow = slow -> next;
+        //     fast = fast -> next -> next;
+        // }
+        // ListNode* secondHalf = reverse(slow->next);
+        // ListNode* firstHalf = head;
+        // ListNode* secondHalfCopy = secondHalf;
+        // while (secondHalf != NULL) {
+        //     if (firstHalf->val != secondHalf->val) {
+        //         reverse(secondHalfCopy);
+        //         return false;
+        //     }
+        //     firstHalf = firstHalf->next;
+        //     secondHalf = secondHalf->next;
+        // }
+        // reverse(secondHalfCopy);
+        // return true;
+        vector<int>arr;
+        ListNode* temp = head;
+        while(temp != NULL){
+            arr.push_back(temp -> val);
+            temp = temp -> next;
         }
-        ListNode* secondHalf = reverse(slow->next);
-        ListNode* firstHalf = head;
-        ListNode* secondHalfCopy = secondHalf;
-        while (secondHalf != NULL) {
-            if (firstHalf->val != secondHalf->val) {
-                reverse(secondHalfCopy);
-                return false;
-            }
-            firstHalf = firstHalf->next;
-            secondHalf = secondHalf->next;
-        }
-        reverse(secondHalfCopy);
-        return true;
+        return isPalindrome(arr);
     }
 };
