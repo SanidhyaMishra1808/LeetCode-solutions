ed27e3401210985113feb919b590d25fece460ba
Sanidhya Mishra <114796705+SanidhyaMishra1808@users.noreply.github.com>
2024-06-18 20:23:13 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (51.79%) - LeetSync

---
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -0,0 +1,58 @@
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    Node* next;
+    Node* random;
+    Node(int _val) {
+        val = _val;
+        next = NULL;
+        random = NULL;
+    }
+};
+*/
+class Solution {
+private:
+    void insertAtTail(Node*& head, Node*& tail, int val) {
+        Node* newnode = new Node(val);
+        if (head == NULL) {
+            head = newnode;
+            tail = newnode;
+            return;
+        } else {
+            tail->next = newnode;
+            tail = newnode;
+        }
+    }
+
+public:
+    Node* copyRandomList(Node* head) {
+        // Step 1: Creating a clone list
+        Node* cloneHead = NULL;
+        Node* cloneTail = NULL;
+        Node* temp = head;
+        while (temp != NULL) {
+            insertAtTail(cloneHead, cloneTail, temp->val);
+            temp = temp->next;
+        }
+        // Step 2: Creating a map named oldToNew
+        unordered_map<Node*, Node*> oldToNew;
+        temp = head;
+        Node* temp2 = cloneHead;
+        while (temp != NULL && temp2 != NULL) {
+            oldToNew[temp] = temp2;
+            temp = temp->next;
+            temp2 = temp2->next;
+        }
+        // Step 3: Updating the random pointers in the clone list
+        temp = head;
+        temp2 = cloneHead;
+        while (temp != NULL) {
+            temp2->random = oldToNew[temp->random];
+            temp = temp->next;
+            temp2 = temp2->next;
+        }
+        return cloneHead;
+    }
+};
