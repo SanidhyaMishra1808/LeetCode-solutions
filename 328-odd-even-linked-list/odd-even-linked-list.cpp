6dc63f05990f5ef57baa30c3f8aa25a9d511b3dc
Sanidhya Mishra <114796705+SanidhyaMishra1808@users.noreply.github.com>
2024-06-18 22:31:24 +0530

    Time: 4 ms (89.95%) | Memory: 14 MB (76.83%) - LeetSync

--- 328-odd-even-linked-list/odd-even-linked-list.cpp
+++ 328-odd-even-linked-list/odd-even-linked-list.cpp
@@ -1,23 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* oddEvenList(ListNode* head) {
-        if(head == NULL || head->next == NULL){
+        //the question is about indices not values
+        if(head == NULL || head -> next == NULL){
             return head;
         }
-        
         ListNode* odd = head;
         ListNode* even = head->next;
-        ListNode* evenHead = even; // Store the start of the even list
-        
+        ListNode* temp = even;   // Store the start of the even list
         while(even != NULL && even->next != NULL){
-            odd->next = odd->next->next;
+            odd -> next = odd -> next -> next;
             odd = odd->next;
-            even->next = even->next->next;
+            even -> next = even -> next -> next;
             even = even->next;
         }
-        
         // Connect the end of the odd list to the start of the even list
-        odd->next = evenHead;
+        odd -> next = temp;
         return head;
     }
 };
