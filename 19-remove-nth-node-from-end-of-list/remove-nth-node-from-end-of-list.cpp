c5fb6888a20085e09156978f63d961374218555f
Sanidhya Mishra <114796705+SanidhyaMishra1808@users.noreply.github.com>
2024-06-19 21:23:10 +0530

    Time: 0 ms (100.00%) | Memory: 13.4 MB (20.18%) - LeetSync

--- 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
+++ 19-remove-nth-node-from-end-of-list/remove-nth-node-from-end-of-list.cpp
@@ -1,36 +1,21 @@
 class Solution {
 public:
-// length of linked list
-    int getLen(ListNode* head){
-        int len = 0;
-        ListNode* temp = head;
-        while(temp != NULL){
-            len++;
-            temp = temp -> next;
-        }
-        return len;
-    }
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        int length = getLen(head);
-        int pos = length - n;
-
-        if (pos == 0) {
-            ListNode* newHead = head->next;
-            delete head;
-            return newHead;
-        }
-        ListNode* current = head;
-        for (int i = 0; i < pos - 1; i++) {
-            if(current == NULL){
-                return head;
+        ListNode* fastp = head;
+        ListNode* slowp = head;
+        for (int i = 0; i < n; i++){
+            fastp = fastp->next;
+            if (fastp == NULL){
+                return head->next;
             }
-            current = current->next;
         }
-        if (current != NULL && current->next != NULL) {
-            ListNode* nodeToRemove = current->next;
-            current->next = current->next->next;
-            delete nodeToRemove;
+        while (fastp->next != NULL) {
+            fastp = fastp->next;
+            slowp = slowp->next;
         }
+        ListNode* delNode = slowp->next;
+        slowp->next = slowp->next->next;
+        delete delNode;
         return head;
     }
 };
