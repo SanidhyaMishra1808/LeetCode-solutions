3a102a6ce9e53904d8e6206cf8c2484f17fba8d2
Sanidhya Mishra <114796705+SanidhyaMishra1808@users.noreply.github.com>
2024-06-30 23:35:36 +0530

    Time: 7 ms (48.22%) | Memory: 13.1 MB (57.56%) - LeetSync

--- 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
+++ 138-copy-list-with-random-pointer/copy-list-with-random-pointer.cpp
@@ -1,17 +1,3 @@
-/*
-// Definition for a Node.
-class Node {
-public:
-    int val;
-    Node* next;
-    Node* random;
-    Node(int _val) {
-        val = _val;
-        next = NULL;
-        random = NULL;
-    }
-};
-*/
 class Solution {
 private:
     void insertAtTail(Node*& head, Node*& tail, int val) {
