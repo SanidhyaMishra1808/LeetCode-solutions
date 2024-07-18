59530c643464c2e787f8b0e49e5ce5000face49d
Sanidhya Mishra <114796705+SanidhyaMishra1808@users.noreply.github.com>
2024-07-19 02:16:23 +0530

    Time: 128 ms (51.10%) | Memory: 64.6 MB (63.74%) - LeetSync

--- 1039-find-the-town-judge/find-the-town-judge.cpp
+++ 1039-find-the-town-judge/find-the-town-judge.cpp
@@ -1,60 +1,24 @@
 class Solution {
 public:
-    int findJudge(int n, vector<vector<int>>& trust) {
-        if (n == 1) return 1;
-
-        // Step 1: Initialize a stack with all people
-        stack<int> stk;
-        for (int i = 1; i <= n; ++i) {
-            stk.push(i);
+    int findJudge(int n, vector<vector<int>>& trust) 
+    {
+        vector<int> trustCount(n + 1, 0);
+        vector<int> trustedByCount(n + 1, 0);
+        
+        for(int i = 0; i < trust.size(); i++)
+        {
+            int a = trust[i][0];
+            int b = trust[i][1];
+            trustCount[a]++;
+            trustedByCount[b]++;
         }
-
-        // Step 2: Find the potential judge
-        while (stk.size() > 1) {
-            int a = stk.top(); stk.pop();
-            int b = stk.top(); stk.pop();
-            if (trusts(a, b, trust)) {
-                stk.push(b);
-            } else {
-                stk.push(a);
-            }
+        
+        for(int i = 1; i <= n; i++)
+        {
+            if(trustCount[i] == 0 && trustedByCount[i] == n - 1)
+                return i;
         }
-
-        // The potential judge
-        int candidate = stk.top();
-        stk.pop();
-
-        // Step 3: Verify the candidate
-        if (isJudge(candidate, n, trust)) {
-            return candidate;
-        }
+        
         return -1;
-    }
-
-private:
-    bool trusts(int a, int b, vector<vector<int>>& trust) {
-        for (auto& pair : trust) {
-            if (pair[0] == a && pair[1] == b) {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    bool isJudge(int candidate, int n, vector<vector<int>>& trust) {
-        int trustedByCount = 0;
-        for (int i = 1; i <= n; ++i) {
-            if (i != candidate && trusts(i, candidate, trust)) {
-                ++trustedByCount;
-            }
-        }
-        bool trustsNoOne = true;
-        for (auto& pair : trust) {
-            if (pair[0] == candidate) {
-                trustsNoOne = false;
-                break;
-            }
-        }
-        return trustedByCount == n - 1 && trustsNoOne;
     }
 };
