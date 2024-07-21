b8966f928f6724c4720cc295c0d1e8a53067c174
Sanidhya Mishra <114796705+SanidhyaMishra1808@users.noreply.github.com>
2024-07-21 14:46:51 +0530

    Time: 20 ms (36.10%) | Memory: 22.2 MB (12.54%) - LeetSync

--- 155-min-stack/min-stack.cpp
+++ 155-min-stack/min-stack.cpp
@@ -2,12 +2,12 @@ class MinStack {
   stack < pair < int, int >> st;
   public:
     void push(int x) {
-      int min;
-      if (st.empty()){
-        st.push({x,x});
-      }else{
-        st.push({x, (x < st.top().second) ? x : st.top().second});
-      }
+        int min;
+        if (st.empty()){
+            st.push({x,x});
+        }else{
+            st.push({x, (x < st.top().second) ? x : st.top().second});
+        }
     }
     void pop() {
         st.pop();
