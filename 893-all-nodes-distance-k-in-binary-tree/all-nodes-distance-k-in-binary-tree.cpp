89b530a3e8535460150afaf01d1d89d486436216
Sanidhya Mishra <114796705+SanidhyaMishra1808@users.noreply.github.com>
2024-09-17 22:42:20 +0530

    Time: 5 ms (48.43%) | Memory: 15 MB (70.63%) - LeetSync

--- 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.cpp
+++ 893-all-nodes-distance-k-in-binary-tree/all-nodes-distance-k-in-binary-tree.cpp
@@ -9,50 +9,52 @@
  */
 class Solution {
 public:
-    unordered_map<TreeNode*, TreeNode*>parent;
-    void inorder(TreeNode* root){
-        if(root == NULL){
-            return ;
+    unordered_map<TreeNode*, TreeNode*> parent;
+    void inorder(TreeNode* root) {
+        if (root == NULL) {
+            return;
         }
-        if(root->left){
+        if (root->left) {
             parent[root->left] = root;
         }
         inorder(root->left);
-        if(root->right){
+        if (root->right) {
             parent[root->right] = root;
         }
         inorder(root->right);
     }
-    void BFS(TreeNode* target, int k, vector<int>& result){
+    void BFS(TreeNode* target, int k, vector<int>& result) {
         queue<TreeNode*> q;
         q.push(target);
 
-        unordered_set<int>visited;
+        unordered_set<int> visited;
         visited.insert(target->val);
 
-        while(!q.empty()){
+        while (!q.empty()) {
             int n = q.size();
-            if(k==0){break;}
+            if (k == 0) {
+                break;
+            }
 
-            while(n--){
+            while (n--) {
                 TreeNode* curr = q.front();
                 q.pop();
-                if(curr->left != NULL && !visited.count(curr->left->val)){
+                if (curr->left != NULL && !visited.count(curr->left->val)) {
                     q.push(curr->left);
                     visited.insert(curr->left->val);
                 }
-                if(curr->right != NULL && !visited.count(curr->right->val)){
+                if (curr->right != NULL && !visited.count(curr->right->val)) {
                     q.push(curr->right);
                     visited.insert(curr->right->val);
                 }
-                if(parent.count(curr) && !visited.count(parent[curr]->val)){
+                if (parent.count(curr) && !visited.count(parent[curr]->val)) {
                     q.push(parent[curr]);
                     visited.insert(parent[curr]->val);
                 }
             }
             k--;
         }
-        while(!q.empty()){
+        while (!q.empty()) {
             TreeNode* temp = q.front();
             q.pop();
             result.push_back(temp->val);
@@ -60,7 +62,7 @@ public:
     }
     vector<int> distanceK(TreeNode* root, TreeNode* target, int k) {
         inorder(root);
-        vector<int>result;
+        vector<int> result;
         BFS(target, k, result);
         return result;
     }
