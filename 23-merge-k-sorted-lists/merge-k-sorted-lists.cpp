ffb4cc0550c82e299f21b2a99872f484e2e78090
Sanidhya Mishra <114796705+SanidhyaMishra1808@users.noreply.github.com>
2024-09-23 22:05:04 +0530

    Time: 11 ms (92.51%) | Memory: 18.4 MB (49.86%) - LeetSync

---
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.cpp
@@ -0,0 +1,35 @@
+struct Compare{
+    bool operator()(ListNode* a, ListNode* b) {
+        return a->val > b->val;
+    }
+};
+
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+    priority_queue<ListNode*, vector<ListNode*>, Compare> pq;
+    int n=lists.size();
+    if(n==0){ 
+        return NULL;
+    }
+
+    for(int i=0;i<n;i++){
+        if(lists[i]!=NULL){
+            pq.push(lists[i]);
+        }
+    }
+
+    ListNode* dummy=new ListNode;
+    ListNode* ans=dummy;
+
+    while(!pq.empty()){
+        auto x=pq.top();
+        pq.pop();
+        ListNode* node= x;
+        ans->next=node;
+        ans=ans->next;
+        if(node->next!=NULL) pq.push({node->next});
+    }
+    return dummy->next;
+    }
+};
