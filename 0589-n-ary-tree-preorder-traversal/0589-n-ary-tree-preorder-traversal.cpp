974cd8e8d61d9e038b8943ef7105d558e4390bc3
Sanidhya Mishra <114796705+SanidhyaMishra1808@users.noreply.github.com>
2024-10-11 12:54:39 +0530

    Time: 11 ms (79.44%), Space: 15.2 MB (82.44%) - LeetHub

--- 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.cpp
+++ 0589-n-ary-tree-preorder-traversal/0589-n-ary-tree-preorder-traversal.cpp
@@ -19,16 +19,18 @@ public:
 */
 class Solution {
 public:
-    void trav(Node* root,vector<int>& ans){
-        if(!root)
-        return;
+    void trav(Node* root, vector<int>& ans) {
+        if (!root) {
+            return;
+        }
         ans.push_back(root->val);
-        for(auto i: root->children)
-        trav(i,ans);
+        for (auto i : root->children){
+            trav(i, ans);
+        }
     }
     vector<int> preorder(Node* root) {
         vector<int> ans;
-        trav(root,ans);
+        trav(root, ans);
         return ans;
     }
 };
