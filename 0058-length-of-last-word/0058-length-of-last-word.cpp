0126301409e9582f7afeacae39c3c9790cfc29bf
Sanidhya Mishra <114796705+SanidhyaMishra1808@users.noreply.github.com>
2024-12-06 15:57:36 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0058-length-of-last-word/0058-length-of-last-word.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int length = s.length();
+        int i = length - 1;
+        int ans = 0;
+        while (i > 0 && s[i] == ' ') {
+            i--;
+        }
+        while (i > 0 && s[i] != ' ') {
+            i--;
+            ans++;
+        }
+        return ans;
+    }
+};
