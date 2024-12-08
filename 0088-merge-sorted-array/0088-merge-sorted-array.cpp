e355ab29d1b03d483a0810330fc708335d8a3879
Sanidhya Mishra <114796705+SanidhyaMishra1808@users.noreply.github.com>
2024-12-08 17:24:39 +0530

    Time: 0 ms (100%), Space: 12.4 MB (6.26%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,35 +1,26 @@
+#include <algorithm> // for sort
+
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        //brute force
-
-        int i = 0;
+        int i = m - 1;
         int j = 0;
-        int index = 0;
-        vector<int> nums3(m + n);
 
-        // Merge nums1 and nums2 into nums3
-        while (i < m && j < n) {
-            if (nums1[i] <= nums2[j]) {
-                nums3[index++] = nums1[i++];
+        // Swap elements to ensure nums1 has the smallest elements
+        while (i >= 0 && j < n) {
+            if (nums1[i] > nums2[j]) {
+                swap(nums1[i--], nums2[j++]);
             } else {
-                nums3[index++] = nums2[j++];
+                break;
             }
         }
+        // Sort both arrays after the swap
+        sort(nums1.begin(), nums1.begin() + m);
+        sort(nums2.begin(), nums2.begin() + n);
 
-        // Copy any remaining elements from nums1
-        while (i < m) {
-            nums3[index++] = nums1[i++];
-        }
-
-        // Copy any remaining elements from nums2
-        while (j < n) {
-            nums3[index++] = nums2[j++];
-        }
-
-        // Copy merged elements back into nums1
-        for (int i = 0; i < m + n; i++) {
-            nums1[i] = nums3[i];
+        // Copy the sorted nums2 back to nums1
+        for (int k = 0; k < n; ++k) {
+            nums1[m + k] = nums2[k];
         }
     }
 };
