174b7d49de25a581cfc20f198b2c67b236fdbb65
Sanidhya Mishra <114796705+SanidhyaMishra1808@users.noreply.github.com>
2024-12-17 00:56:08 +0530

    Time: 7 ms (7.18%), Space: 11.1 MB (82.64%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,97 +1,15 @@
-class TrieNode {
-public:
-    char data;
-    TrieNode* children[26];
-    int childCount;
-    bool isTerminal;
-
-    // Constructor
-    TrieNode(char ch) {
-        data = ch;
-        for (int i = 0; i < 26; i++) {
-            children[i] = NULL;
-        }
-        childCount = 0;
-        isTerminal = false;
-    }
-};
-
-class Trie {
-public:
-    TrieNode* root;
-
-    // Constructor
-    Trie() {
-        root = new TrieNode('\0');
-    }
-
-    void insertUtil(TrieNode* root, const string& word) {
-        // Base case
-        if (word.length() == 0) {
-            root->isTerminal = true;
-            return;
-        }
-
-        // Assuming word will be lowercase
-        int index = word[0] - 'a';
-        TrieNode* child;
-
-        // Present
-        if (root->children[index] != NULL) {
-            child = root->children[index];
-        }
-        // Absent
-        else {
-            child = new TrieNode(word[0]);
-            root->childCount++;
-            root->children[index] = child;
-        }
-
-        // Recursion
-        insertUtil(child, word.substr(1));
-    }
-
-    void insertWord(const string& word) {
-        insertUtil(root, word);
-    }
-
-    void lcp(const string& str, string& ans) {
-        TrieNode* current = root;
-        for (int i = 0; i < str.length(); i++) {
-            char ch = str[i];
-            if (current->childCount == 1) {
-                ans.push_back(ch);
-                // Move forward
-                int index = ch - 'a';
-                current = current->children[index];
-            } else {
-                break;
-            }
-            if (current->isTerminal) {
-                break;
-            }
-        }
-    }
-};
-
 class Solution {
 public:
     string longestCommonPrefix(vector<string>& strs) {
-        if (strs.empty()) return "";
-
-        // Check for empty strings
-        for (const string& str : strs) {
-            if (str.empty()) return "";
+        if (strs.empty()){
+            return "";
         }
-
-        Trie* t = new Trie();
-        for (int i = 0; i < strs.size(); i++) {
-            t->insertWord(strs[i]);
+        string prefix = strs[0];
+        for (string s : strs){
+            while (s.find(prefix) != 0){
+                prefix = prefix.substr(0, prefix.length() - 1);
+            }
         }
-
-        string first = strs[0];
-        string ans = "";
-        t->lcp(first, ans);
-        return ans;
+        return prefix;
     }
 };
