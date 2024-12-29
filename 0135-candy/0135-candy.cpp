dfb08185b453969ae650b0ac30c4b3c500829191
Sanidhya Mishra <114796705+SanidhyaMishra1808@users.noreply.github.com>
2024-12-29 20:59:49 +0530

    Time: 0 ms (100%), Space: 21.8 MB (22%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -2,17 +2,22 @@ class Solution {
 public:
     int candy(vector<int>& ratings) {
         int n = ratings.size();
-        int candy = 0;
-        for(int i=0; i<n; i++){
-            candy += 1;
-            // Check left and right neighbors
-            if (i > 0 && ratings[i - 1] < ratings[i]) {
-                candy += 1;
-            }
-            if (i < n - 1 && ratings[i] > ratings[i + 1]) {
-                candy += 1;
+        vector<int> candies(n, 1);
+        
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
             }
         }
-        return candy;
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1]) {
+                candies[i] = max(candies[i], candies[i + 1] + 1);
+            }
+        }
+        int totalCandies = 0;
+        for (int i = 0; i < n; i++) {
+            totalCandies += candies[i];
+        }
+        return totalCandies;
     }
 };
