09f30dc6d06640f293dc0e0011ffdf16ca9cdada
Sanidhya Mishra <114796705+SanidhyaMishra1808@users.noreply.github.com>
2025-01-05 15:08:02 +0530

    Time: 19 ms (25.97%), Space: 17.4 MB (22.64%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.cpp
+++ 0006-zigzag-conversion/0006-zigzag-conversion.cpp
@@ -6,7 +6,8 @@ public:
         }     
         int idx = 0, d = 1;
         vector<vector<char>> rows(numRows);
-        // +d means down and -d means up; idx+=d shows where to put the values
+        // +d means down and -d means up; 
+        // idx += d shows where to put the values
         for (char c : s) {
             rows[idx].push_back(c);
             if (idx == 0) {
@@ -17,8 +18,8 @@ public:
             idx += d;
         }
         string result;
-        for (int i = 0; i < rows.size(); ++i) {
-            for (int j = 0; j < rows[i].size(); ++j) {
+        for (int i = 0; i < rows.size(); i++) {
+            for (int j = 0; j < rows[i].size(); j++) {
                 result += rows[i][j];
             }
         }
