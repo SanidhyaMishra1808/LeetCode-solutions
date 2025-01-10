c0c3207693de315a1d04f2de3513e36a8082bf70
Sanidhya Mishra <114796705+SanidhyaMishra1808@users.noreply.github.com>
2025-01-10 18:34:47 +0530

    Time: 0 ms (100%), Space: 7.8 MB (83.72%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int helper(int n){
+        int ans = 0;
+        while(n){
+            int temp = n % 10;
+            ans += (temp * temp);
+            n /= 10;
+        }
+        return ans;
+    }
+    bool isHappy(int n) {
+        int slow = n, fast = helper(n);
+        while(slow != fast){
+            slow = helper(slow);
+            fast = helper(helper(fast));
+        }
+        return slow == 1;
+    }
+};
