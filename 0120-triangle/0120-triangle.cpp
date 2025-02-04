2758cbe3299196123f1e74a80d825bd2c52dc8d5
Sanidhya Mishra <114796705+SanidhyaMishra1808@users.noreply.github.com>
2025-02-04 18:08:38 +0530

    Time: 3 ms (14.57%), Space: 12.6 MB (83.18%) - LeetHub

--- 0120-triangle/0120-triangle.cpp
+++ 0120-triangle/0120-triangle.cpp
@@ -1,13 +1,22 @@
 class Solution {
 public:
     int minimumTotal(vector<vector<int>>& triangle) {
+        // Get the number of rows in the triangle
         int row = triangle.size();
-        vector<int> memo = triangle[row-1];
-        for (int r = row-2; r >= 0; r--) {
-            for (int c = 0; c <= r; c++) {
-                memo[c] = min(memo[c], memo[c+1]) + triangle[r][c];
+
+        // Create a temporary vector initialized with the values of the last row
+        vector<int> temp = triangle[row - 1];
+
+        // Traverse the triangle from the second last row up to the top
+        for (int i = row - 2; i >= 0; i--) {
+            // Iterate through each element in the current row
+            for (int j = 0; j <= i; j++) { // Issue here: 'r' is undeclared and incorrect
+                // Update temp[j] with the minimum path sum from the current position
+                temp[j] = min(temp[j], temp[j + 1]) + triangle[i][j];
             }
         }
-        return memo[0];        
+
+        // Return the minimum path sum starting from the top of the triangle
+        return temp[0];
     }
 };
