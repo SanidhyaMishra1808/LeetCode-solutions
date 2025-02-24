82c7c302d6a297216bb68e40f45dbb2b67363a1b
Sanidhya Mishra <114796705+SanidhyaMishra1808@users.noreply.github.com>
2025-02-24 14:34:10 +0530

    Time: 2 ms (15.93%), Space: 9.8 MB (33.44%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<vector<int>> generate(int numRows) {
+        // in 2d matrix we can not initaialize all the element with 1 by using <int>>ans(numRows, 1) because each initializes row with 1 nit the 1st vector inside the main vector;
+        vector<vector<int>> ans(numRows);
+        for (int i = 0; i < numRows; i++) {
+            ans[i].resize(i + 1, 1);
+            for (int j = 1; j < i; j++) {
+                ans[i][j] = ans[i - 1][j - 1] + ans[i - 1][j];
+            }
+        }
+        return ans;
+    }
+};
