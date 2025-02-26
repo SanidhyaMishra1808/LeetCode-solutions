9cfd27ab8dd28b0cc9a349c84c58330785f27d28
Sanidhya Mishra <sanidhyamishra1808@gmail.com>
2025-02-26 21:41:43 +0530

    Time: 3 ms (89.82%), Space: 23.8 MB (79.89%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,29 +1,28 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
+        // similar to previous soln but with O(N) instead of O(2N) Log(N)
+        int n = intervals.size();
+
         // 1. Sort the matrix by 1st element
         sort(intervals.begin(), intervals.end());
 
         // 2. create additional list to append into
         vector<vector<int>> intervals2;
 
-        // 3. adding in list
-        int n = intervals.size();
+        // 3. adding in new list
         for(int i=0; i<n; i++){
-            int start = intervals[i][0];
-            int end = intervals[i][1];
-            if(!intervals2.empty() && end <= intervals2.back()[1]){
-                continue;
+
+        // if list is empty or the element we are pushing does not overlap
+            if(intervals2.empty() || intervals[i][0] > intervals2.back()[1]){
+                intervals2.push_back(intervals[i]);
+            }else{
+        // if it overlaps than just keep [(this),__] and replace with the max element at [x, _?_] here
+                intervals2.back()[1] = max(intervals2.back()[1], intervals[i][1]);
             }
-            for(int j=0; j<n; j++){
-                if(intervals[j][0] <= end){
-                    end = max(end, intervals[j][1]);
-                }else{
-                    break;
-                }
-            }
-            intervals2.push_back({start, end});
         }
+
+        // 4. return the ans list
         return intervals2;
     }
 };
