04672ff88d8b18575f833923daf4f85bf254b9e3
Sanidhya Mishra <sanidhyamishra1808@gmail.com>
2025-02-27 14:53:11 +0530

    Time: 0 ms (100%), Space: 12.3 MB (39.57%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.cpp
+++ 0088-merge-sorted-array/0088-merge-sorted-array.cpp
@@ -1,26 +1,24 @@
-#include <algorithm> // for sort
-
 class Solution {
 public:
     void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
-        int i = m - 1;
-        int j = 0;
-
-        // Swap elements to ensure nums1 has the smallest elements
-        while (i >= 0 && j < n) {
-            if (nums1[i] > nums2[j]) {
-                swap(nums1[i--], nums2[j++]);
-            } else {
-                break;
+        // if you notic nums1 always have space for the elemet=nts of nums2 even [0] thus filling from by backwards would be the right approach for that O(1) space
+        int i = m-1, j = n-1, k = m+n-1;
+        // accessing from backwards
+        while(i>=0 && j>=0){
+            if(nums1[i] > nums2[j]){
+                nums1[k] = nums1[i];       // as nums1[i] bada he or hamm piche se bhar rahe he
+                i--;
+            }else{
+                nums1[k] = nums2[j];
+                j--;
             }
+            k--;
         }
-        // Sort both arrays after the swap
-        sort(nums1.begin(), nums1.begin() + m);
-        sort(nums2.begin(), nums2.begin() + n);
-
-        // Copy the sorted nums2 back to nums1
-        for (int k = 0; k < n; ++k) {
-            nums1[m + k] = nums2[k];
+        // ab agar nums2 me element bache reh jaye to unhe nums1 me simply put kar dena he
+        while(j >= 0){
+            nums1[k] = nums2[j];
+            j--;
+            k--;
         }
     }
 };
