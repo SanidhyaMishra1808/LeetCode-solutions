04fd14918186c7500ddc46024cac79d6c182a1e7
Sanidhya Mishra <sanidhyamishra1808@gmail.com>
2025-03-03 16:06:54 +0530

    Time: 0 ms (100%), Space: 19.9 MB (60.48%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.cpp
+++ 0229-majority-element-ii/0229-majority-element-ii.cpp
@@ -1,44 +1,46 @@
 class Solution {
 public:
     vector<int> majorityElement(vector<int>& nums) {
+        int n = nums.size();
+        // if(n/3 < 1){
+        //     return nums;
+        // }
+        vector<int> ans;
         int count1 = 0;
-        int majo1 = 0;
         int count2 = 0;
-        int majo2 = 0;
-        for (int i = 0; i < nums.size(); i++) {
-            if (nums[i] == majo1) {
+        int major1 = 0;
+        int major2 = 0;
+        for (int i = 0; i < n; i++) {
+            if (nums[i] == major1) {
                 count1++;
-            } else if (nums[i] == majo2) {
+            } else if (nums[i] == major2) {
                 count2++;
             } else if (count1 == 0) {
-                majo1 = nums[i];
+                major1 = nums[i];
                 count1 = 1;
             } else if (count2 == 0) {
-                majo2 = nums[i];
+                major2 = nums[i];
                 count2 = 1;
             } else {
                 count1--;
                 count2--;
             }
         }
-        // Verification part
-        vector<int> result;
         int freq1 = 0;
         int freq2 = 0;
         for (int& i : nums) {
-            if (i == majo1) {
+            if (i == major1) {
                 freq1++;
-            } else if (i == majo2) {
+            } else if (i == major2) {
                 freq2++;
             }
         }
-        int n = nums.size();
         if (freq1 > n / 3) {
-            result.push_back(majo1);
+            ans.push_back(major1);
         }
         if (freq2 > n / 3) {
-            result.push_back(majo2);
+            ans.push_back(major2);
         }
-        return result;
+        return ans;
     }
 };
