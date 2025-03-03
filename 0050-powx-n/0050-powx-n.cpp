cbb1cb076bccce0df573ffddbbde1032ab0aeddf
Sanidhya Mishra <sanidhyamishra1808@gmail.com>
2025-03-03 15:18:17 +0530

    Time: 0 ms (100%), Space: 8.7 MB (15.55%) - LeetHub

--- 0050-powx-n/0050-powx-n.cpp
+++ 0050-powx-n/0050-powx-n.cpp
@@ -1,18 +1,25 @@
 class Solution {
 public:
     double myPow(double x, int n) {
-        if (n == 0) return 1;
-        if (x == 1) return 1;
-        long long p = n; 
+        // case for when either x or n = 0,1
+        if (x == 0)
+            return 0;
+        if (n == 0)
+            return 1;
+
+        // now their lies two main cases
+        // 1. for negative power
+        long long p = n;
         double ans = 1;
         if (p < 0) {
-            x = 1 / x;
+            x = 1 / x; // 5^(-2) == 1 / 5^(2)
             if (p == INT_MIN) {
-                p++; 
+                p++;
                 ans *= x;
             }
             p = -p;
         }
+        // 2. for positive powers
         while (p > 0) {
             if (p % 2 == 1) {
                 ans *= x;
