80da05ed4cdaac89e80db3338fd829712c084b67
Sanidhya Mishra <sanidhyamishra1808@gmail.com>
2025-03-22 16:58:19 +0530

    Time: 0 ms (100%), Space: 15.1 MB (25.11%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -1,20 +1,32 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
     ListNode* removeNthFromEnd(ListNode* head, int n) {
-        ListNode* fast = head;
-        ListNode* slow = head;
-        for (int i = 0; i < n; i++){
-            fast = fast->next;
-            if (fast == NULL){
+        ListNode* fwd = head;
+        ListNode* bck = head;
+
+        // ab me fwd node ko normal node se nth step aage rakhung jis se jab mera fwd node NULL reach karega tab mera bck node us position pe hoga jo mujhe delete karni hogi
+        for(int i=0; i<n; i++){
+            fwd = fwd -> next;
+            if(fwd == NULL){
                 return head->next;
             }
         }
-        while (fast->next != NULL) {
-            fast = fast->next;
-            slow = slow->next;
+        while(fwd->next != NULL){
+            fwd = fwd ->next;
+            bck = bck ->next;
         }
-        ListNode* delNode = slow->next;
-        slow->next = slow->next->next;
+        ListNode* delNode = bck->next;
+        bck->next = bck->next->next;
         delete delNode;
         return head;
     }
