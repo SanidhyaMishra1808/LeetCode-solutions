9db23b3085ad86b95c3376a20ddeb92b0a5d6555
Sanidhya Mishra <sanidhyamishra1808@gmail.com>
2025-03-31 11:29:56 +0530

    Time: 5 ms (39.35%), Space: 126.4 MB (34.65%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.cpp
@@ -1,61 +1,22 @@
-#include<vector>
 class Solution {
-private:
-bool isPalindrome(vector<int>&arr){
-    int s = 0;
-    int n = arr.size();
-    int e = n-1;
-    while(s<e){
-        if(arr[s] != arr[e]){
+public:
+    ListNode* front;
+    bool checkPalindrome(ListNode* curr) {
+        if (curr == NULL){
+            return true;
+        }
+        bool isPalin = checkPalindrome(curr->next);
+        if(!isPalin){
             return false;
         }
-        s++;
-        e--;
-    }
-    return true;
-}
-public:
-// ListNode* reverse(ListNode* head) {
-//     if(head == NULL || head -> next == NULL){
-//             return head;
-//         }
-//         ListNode* curr = head;
-//         ListNode* forward = NULL;
-//         ListNode* prev = NULL;
-//         while(curr != NULL){
-//             forward = curr -> next;
-//             curr -> next = prev;
-//             prev = curr;
-//             curr = forward;
-//         }
-//         return prev;
-// }
-    bool isPalindrome(ListNode* head) {
-        // ListNode* slow = head;
-        // ListNode* fast = head;
-        // while(fast -> next != NULL && fast -> next -> next != NULL){
-        //     slow = slow -> next;
-        //     fast = fast -> next -> next;
-        // }
-        // ListNode* secondHalf = reverse(slow->next);
-        // ListNode* firstHalf = head;
-        // ListNode* secondHalfCopy = secondHalf;
-        // while (secondHalf != NULL) {
-        //     if (firstHalf->val != secondHalf->val) {
-        //         reverse(secondHalfCopy);
-        //         return false;
-        //     }
-        //     firstHalf = firstHalf->next;
-        //     secondHalf = secondHalf->next;
-        // }
-        // reverse(secondHalfCopy);
-        // return true;
-        vector<int>arr;
-        ListNode* temp = head;
-        while(temp != NULL){
-            arr.push_back(temp -> val);
-            temp = temp -> next;
+        if(front->val != curr->val){
+            return false;
         }
-        return isPalindrome(arr);
+        front = front->next;
+        return isPalin;
+    }
+    bool isPalindrome(ListNode* head) {
+        front = head;
+        return checkPalindrome(head);
     }
 };
