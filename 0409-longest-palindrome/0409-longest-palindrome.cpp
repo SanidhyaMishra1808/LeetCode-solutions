9b27e051e08bfd3a0b19fdc05ed925ccb4be458e
Sanidhya Mishra <sanidhyamishra1808@gmail.com>
2025-04-07 22:31:24 +0530

    Time: 0 ms (100%), Space: 9.3 MB (62.02%) - LeetHub

--- 0409-longest-palindrome/0409-longest-palindrome.cpp
+++ 0409-longest-palindrome/0409-longest-palindrome.cpp
@@ -1,24 +1,36 @@
 class Solution {
 public:
     int longestPalindrome(string s) {
-        unordered_map<char, int> freq;
-        for (char c : s) {
-            freq[c]++;
-        }
-        int ans = 0;
-        bool odd_found = false;
-        for (auto& pair : freq) {
-            int count = pair.second;
-            if (count % 2 == 0) {
-                ans += count;
-            } else {
-                ans += count - 1; // jab ek hi char ka total cnt odd ho jese ccc but lngest pal possible ho
-                odd_found = true;
+        vector<int> lower(26, 0);
+        vector<int> upper(26, 0);
+
+        int ans = 0; 
+        int odd = 0;
+
+        for (char ch : s) {
+            if (islower(ch)) {
+                lower[ch - 'a']++;
+            } else if (isupper(ch)) {
+                upper[ch - 'A']++;
             }
         }
-        if (odd_found) {
-            ans += 1;
+
+        for (int i = 0; i < 26; i++) {
+            if (lower[i] % 2 == 0) {
+                ans += lower[i];
+            } else {
+                ans += lower[i] - 1;
+                odd = 1;
+            }
+
+            if (upper[i] % 2 == 0) {
+                ans += upper[i];
+            } else {
+                ans += upper[i] - 1;
+                odd = 1;
+            }
         }
-        return ans;
+
+        return ans + odd;
     }
 };
