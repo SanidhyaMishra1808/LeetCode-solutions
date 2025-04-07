c3373de69315ada1fc7c6a6a0577290199f09ab3
Sanidhya Mishra <sanidhyamishra1808@gmail.com>
2025-04-07 23:32:27 +0530

    Time: 0 ms (100%), Space: 22.8 MB (61.05%) - LeetHub

--- 0135-candy/0135-candy.cpp
+++ 0135-candy/0135-candy.cpp
@@ -3,21 +3,20 @@ public:
     int candy(vector<int>& ratings) {
         int n = ratings.size();
         vector<int> candies(n, 1);
-        
-        for (int i = 1; i < n; i++) {
-            if (ratings[i] > ratings[i - 1]) {
-                candies[i] = candies[i - 1] + 1;
+        for(int i=1; i<n; i++){
+            if(ratings[i] > ratings[i-1]){
+                candies[i] = candies[i-1] + 1;
             }
         }
-        for (int i = n - 2; i >= 0; i--) {
-            if (ratings[i] > ratings[i + 1]) {
-                candies[i] = max(candies[i], candies[i + 1] + 1);
+        for(int i=n-2; i>=0; i--){
+            if(ratings[i] > ratings[i+1]){
+                candies[i] = max(candies[i], candies[i+1] + 1);
             }
         }
-        int totalCandies = 0;
-        for (int i = 0; i < n; i++) {
-            totalCandies += candies[i];
+        int totalcandies = 0;
+        for(int i=0; i<n; i++){
+            totalcandies += candies[i];
         }
-        return totalCandies;
+        return totalcandies;
     }
 };
