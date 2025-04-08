d7a5356bd8cb99f353452dcd9299d81e33b75181
Sanidhya Mishra <sanidhyamishra1808@gmail.com>
2025-04-09 00:10:24 +0530

    Time: 338 ms (5.21%), Space: 21.9 MB (14.43%) - LeetHub

--- 0057-insert-interval/0057-insert-interval.cpp
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -2,18 +2,34 @@ class Solution {
 public:
     vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
         int i = 0;
-        while(i<intervals.size()){
-            if(intervals[i][1] < newInterval[0]){
+        while(i < intervals.size()) {
+
+            // Agar current interval ka end newInterval ke start se chhota hai
+            // to iska matlab overlap nahi hai, aage badho
+            if(intervals[i][1] < newInterval[0]) {
                 i++;
-            }else if(intervals[i][0] > newInterval[1]){
-                intervals.insert(intervals.begin() + i, newInterval);
-                return intervals;
-            }else{
-                newInterval[0] = min(newInterval[0], intervals[i][0]); 
+            }
+
+            // Agar current interval ka start newInterval ke end se bada hai
+            // to iska matlab is position par hum newInterval insert kar sakte hai
+            else if(intervals[i][0] > newInterval[1]) {
+                intervals.insert(intervals.begin() + i, newInterval); // insert karo
+                return intervals; // aur return kar do result
+            }
+
+            // Agar intervals overlap karte hain
+            else {
+                // newInterval ka start aur end update karo minimum aur maximum ke according
+                newInterval[0] = min(newInterval[0], intervals[i][0]);
                 newInterval[1] = max(newInterval[1], intervals[i][1]);
-                intervals.erase(intervals.begin() + i); 
+
+                // current interval ko hata do kyunki ab wo merge ho chuka hai
+                intervals.erase(intervals.begin() + i);
+                // i ko increase mat karo kyunki naye updated newInterval ko dubara isi position par check karna hai
             }
         }
+
+        // Agar newInterval ab tak insert nahi hua, to end mein push kar do
         intervals.push_back(newInterval);
         return intervals;
     }
