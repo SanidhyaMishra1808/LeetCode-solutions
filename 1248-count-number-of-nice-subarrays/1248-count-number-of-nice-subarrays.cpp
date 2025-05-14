984b5f28baa88b3209166db8641e1649fd86eafb
Sanidhya Mishra <sanidhyamishra1808@gmail.com>
2025-05-14 15:22:10 +0530

    Time: 3 ms (88.34%), Space: 71.5 MB (69.08%) - LeetHub

--- 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
+++ 1248-count-number-of-nice-subarrays/1248-count-number-of-nice-subarrays.cpp
@@ -1,25 +1,28 @@
 class Solution {
 public:
     int numberOfSubarrays(vector<int>& nums, int k) {
-        int oddCnt = 0;
-        int prevCnt = 0;
+        int oddCnt = 0;     // Count of odd numbers in the current window
+        int prevCnt = 0;    // Count of valid subarrays ending at current j with exactly k odd numbers
         int ans = 0;
 
         int i = 0;
         int j = 0;
         while (j < nums.size()) {
             if (nums[j] % 2 != 0) {
-                oddCnt++;
-                prevCnt = 0;
+                oddCnt++;      // Increment odd count when an odd number is found
+                prevCnt = 0;   // Reset prevCnt as a new window may start from here
             }
+
+            // When there are exactly k odd numbers in the window
             while (oddCnt == k) {
-                prevCnt++;
+                prevCnt++;     // Count number of valid starting points for subarrays ending at j
                 if (i < nums.size() && nums[i] % 2 == 1) {
-                    oddCnt--;
+                    oddCnt--;  // Shrink window from left if an odd is removed
                 }
                 i++;
             }
-            ans += prevCnt;
+
+            ans += prevCnt;    // Add all valid subarrays ending at j
             j++;
         }
         return ans;
