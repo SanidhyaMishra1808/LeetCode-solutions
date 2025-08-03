e9427298687aa7b8cf6ff58ec25942e0065c0b07
Sanidhya Mishra <sanidhyamishra1808@gmail.com>
2025-08-03 16:11:34 +0530

    Time: 46 ms (36.18%), Space: 75 MB (77.23%) - LeetHub

--- 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
+++ 0904-fruit-into-baskets/0904-fruit-into-baskets.cpp
@@ -2,12 +2,12 @@ class Solution {
 public:
     int totalFruit(vector<int>& fruits) {
         // understand problem as to find the longest subarray with at most 2 distinct elements
-        int i = 0, j = 0, maxL = 0;
+        int i = 0, j = 0, maxL = -1;
         unordered_map<int , int>mp;
 
         while (j < fruits.size()){
             mp[fruits[j]]++;
-            if(mp.size() > 2){
+            while(mp.size() > 2){
                 mp[fruits[i]]--;
                 if(mp[fruits[i]] == 0){
                     mp.erase(fruits[i]);
